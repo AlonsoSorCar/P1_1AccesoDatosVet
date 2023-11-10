61cf4e06c35229e353e764bd077ca32630859d76
AlonsoSorCar <alonso.sorcar@educa.jcyl.es>
2023-11-10 18:33:44 +0100

    Commit final2

--- src/main/resources/BaseDatos/bdVeterinario.sql
+++ src/main/resources/BaseDatos/bdVeterinario.sql
@@ -3,14 +3,24 @@ create database veterinario;
 use veterinario;
 
 create table mascotas(
-	id int unsigned auto_increment primary key,
-    nombre varchar(45),
-    raza varchar(45),
-    peso decimal(4,2),
-    fechaN date,
-    causaConsulta varchar(45),
-    otros text
+                         id int unsigned auto_increment primary key,
+                         nombre varchar(45),
+                         raza varchar(45),
+                         peso decimal(4,2),
+                         fechaN date,
+                         causaConsulta varchar(45),
+                         otros text
 );
+
+
+CREATE TABLE usuarios (
+                          id int auto_increment primary key,
+                          nombre varchar(255),
+                          salt varchar(64), -- Campo para almacenar la sal
+                          contrasena_hash varchar(64) -- el hashusuarios
+);
+
+
 INSERT INTO `veterinario`.`mascotas` (`nombre`, `raza`, `peso`, `fechaN`, `causaConsulta`, `otros`) VALUES ('zarpas', 'gato', '4.3', '2017-02-14', 'pata rota', 'agresivo');
 INSERT INTO `veterinario`.`mascotas` (`nombre`, `raza`, `peso`, `fechaN`, `causaConsulta`, `otros`) VALUES ('bigotes', 'gato', '5.2', '2021-07-24', 'Neumonia', '');
 INSERT INTO `veterinario`.`mascotas` (`nombre`, `raza`, `peso`, `fechaN`, `causaConsulta`, `otros`) VALUES ('max', 'perro', '12.9', '2022-07-24', 'corte pelo', 'usa bozal');
